3b827b13a34749b84b24800141d4b36d92f66b3a
Ibtihaj Adham <92644947+Ibtihaj7@users.noreply.github.com>
2023-08-06 12:52:17 +0300

    Time: 17 ms (68.72%), Space: 14.4 MB (81.90%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.swift
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.swift
@@ -1,18 +1,17 @@
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        var charSet = Set<Character>()
+        var charIndexMap = [Character: Int]()
         var maxLength = 0
         var left = 0
         let chars = Array(s)
 
         for right in 0..<s.count {
             let currentChar = chars[right]
-            while charSet.contains(currentChar) {
-                charSet.remove(chars[left])
-                left += 1
+            if let prevIndex = charIndexMap[currentChar], prevIndex >= left {
+                left = prevIndex + 1
             }
 
-            charSet.insert(currentChar)
+            charIndexMap[currentChar] = right
             maxLength = max(maxLength, right - left + 1)
         }
 
