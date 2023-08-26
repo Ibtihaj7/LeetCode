80a3ecbf525c21b2ddb91bd5c0318678aa1cd709
Ibtihaj Adham <92644947+Ibtihaj7@users.noreply.github.com>
2023-08-26 10:31:47 +0300

    Time: 349 ms (91.81%), Space: 14.1 MB (78.41%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var maxReach = 0
+
+        for i in 0..<nums.count {
+            if i > maxReach {
+                return false 
+            }
+            maxReach = max(maxReach, i + nums[i]) 
+            if maxReach >= nums.count - 1 {
+                return true
+            }
+        }
+        return false
+    }
+}
